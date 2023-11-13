5545558d4f869b6792f24dfc7cbb59e6ed307cde
navsubb <71675266+navsubb@users.noreply.github.com>
2023-11-13 15:57:46 -0800

    Add navbar component according to Figma spec

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,19 @@
-// THIS IS FOR PRANAV's NAVBAR
+import "./index.css"
 
 const Navbar = () => {
-
-}
+  return (
+    <div className="navbar">
+      <div className="logo-container">
+        <img src="logo.png" alt="Logo" className="logo" />
+      </div>
+      <div className="nav-buttons">
+        <button className="nav-button">Library</button>
+        <button className="nav-button">Accounts</button>
+        <button className="nav-button">Features</button>
+        <button className="nav-button login-button">Login</button>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
