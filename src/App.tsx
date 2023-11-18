f870bd57b303fa4ab60d74c5e0bad89386f0c150
joanne <tranjoanne@berkeley.edu>
2023-11-17 18:51:54 -0800

    removed modified file maybe idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-// In App.tsx
 import React from 'react';
-import Home from './pages/Home/Home';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div>
-      <Home />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
