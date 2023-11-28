1fcd18ed81ef98ff5e31f0c1b128a5f32f1c200a
DanielSamony <96964758+danielsamony@users.noreply.github.com>
2023-11-27 19:18:28 -0800

    Re-add app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import VideoListing from './pages/VideoListing/VideoListing';
 
 function App() {
   return (
-    <VideoListing />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
