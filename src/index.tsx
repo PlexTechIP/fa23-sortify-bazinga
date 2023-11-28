180f29db9b9d7050340f629808445a36c4eda316
Eugene Chao <epchao@berkeley.edu>
2023-11-27 17:16:28 -0800

    Add react-router dependency to repo. Set homepage. Define example routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Dashboard from './pages/Dashboard/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }, 
+  // {
+  //   path: "/dashboard",
+  //   element: <Dashboard />
+  // },
+  // {
+  //   path: "*",
+  //   element: <NotFound />
+  // }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
