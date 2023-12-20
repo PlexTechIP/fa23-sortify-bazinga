d45a453df00207fad4db9277f0315cb0c202167d
Eugene Chao <epchao@berkeley.edu>
2023-12-20 15:03:54 -0800

    Made a basic home page that will be expanded on later on

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,40 @@
-// In Home.tsx
 import React from 'react';
-import './Home.css'; 
+import './Home.css';
+import Navbar from '../../components/Navbar';
 
 const Home: React.FC = () => {
   return (
-    <div className="background">
-      <h1 className="heroTitle">Sortify</h1>
-      <h3 className='heroDesc'>Video Organizer Hub</h3>
-    </div>
+    <>
+      <div className="hero-bg">
+        <Navbar />
+        <div className="hero">
+          <div className="hero-content">
+            <h1 className="hero-title">Sortify</h1>
+            <h3 className='hero-desc'>Video Organizer Hub</h3>
+          </div>
+          <div className="hero-media">
+            <p>this is an img.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="content-bg">
+        <div className="content">
+          <h1>How it works</h1>
+          <ol className="feature-steps">
+            <li>Log in with your Google account</li>
+            <li>Link your TikTok, Instagram, and/or YouTube</li>
+            <li>Search through all of your saved content</li>
+          </ol>
+        </div>
+      </div>
+
+      <div className="footer-bg">
+        <div className="footer">
+          Made by BAZINGA
+        </div>
+      </div>
+    </>
   );
 };
 
